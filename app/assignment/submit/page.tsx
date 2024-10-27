b0d2bf1dbd47f39191065482b9da025580d6dedf
sourav0299 <105085605+sourav0299@users.noreply.github.com>
2024-10-27 17:20:41 +0530

    Refractor: UI fix of login and navbar

--- app/assignment/submit/page.tsx
+++ app/assignment/submit/page.tsx
@@ -93,99 +93,117 @@ const SubmitForm: React.FC = () => {
   };
 
   return (
-      <div className="max-w-xl mx-auto p-4">
-        <Toaster position="top-center" reverseOrder={false} />
-      <h1 className="text-2xl font-bold mb-6">Submit Assignment</h1>
-
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label htmlFor="name" className="block mb-2">Your Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-            className="w-full p-2 border rounded"
-          />
+    <div className="mx-auto h-screen bg-gray-500">
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="flex items-center justify-around py-10">
+        <div className="bg-white shadow rounded w-[751px] h-[562px]">
+          <div className="">
+            <h1 className="">Submit Assignment</h1>
+          </div>
         </div>
-
-        <div>
-          <label htmlFor="email" className="block mb-2">Your Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            className="w-full p-2 border rounded"
-          />
+        <div className="">
+          <div className="">
+            <h1 className=""></h1>
+          </div>
+          <div className="">
+            <p className=""></p>
+          </div>
         </div>
-
-        <div>
-          <label htmlFor="phone" className="block mb-2">Phone Number</label>
-          <input
-            type="text"
-            id="phone"
-            name="phone"
-            value={formData.phone}
-            onChange={handleInputChange}
-            className="w-full p-2 border rounded"
-            required
-          />
-        </div>
-              
-        <div>
-          <label htmlFor="position" className="block mb-2">Applied for position:</label>
-          <input
-            type="text"
-            id="position"
-            name="position"
-            value={formData.position}
-            onChange={handleInputChange}
-            className="w-full p-2 border rounded"
-            required
-          />
-        </div>
-
-        <div>
-          <label htmlFor="figmaLink" className="block mb-2">Figma Link</label>
-          <input
-            type="url"
-            id="figmaLink"
-            name="figmaLink"
-            value={formData.figmaLink}
-            onChange={handleInputChange}
-            className="w-full p-2 border rounded"
-          />
-        </div>
-
-        <div>
-          <label htmlFor="googleDriveLink" className="block mb-2">Google Drive Link</label>
-          <input
-            type="url"
-            id="googleDriveLink"
-            name="googleDriveLink"
-            value={formData.googleDriveLink}
-            onChange={handleInputChange}
-            className="w-full p-2 border rounded"
-          />
-        </div>
-
-        <button
-          type="submit"
-          className={`px-4 py-2 rounded ${
-            submissionExists 
-              ? "bg-gray-400 cursor-not-allowed" 
-              : "bg-blue-500 hover:bg-blue-600"
-          } text-white`}
-          title={submissionExists ? "You have already submitted an assignment. Only one submission is allowed." : ""}
-          disabled={submissionExists}
-        >
-          {submissionExists ? "Already Submitted" : "Submit"}
-        </button>
-      </form>
+      </div>
     </div>
+    //   <div className="max-w-xl mx-auto p-4">
+    //     <Toaster position="top-center" reverseOrder={false} />
+    //   <h1 className="text-2xl font-bold mb-6">Submit Assignment</h1>
+
+    //   <form onSubmit={handleSubmit} className="space-y-4">
+    //     <div>
+    //       <label htmlFor="name" className="block mb-2">Your Name</label>
+    //       <input
+    //         type="text"
+    //         id="name"
+    //         name="name"
+    //         value={formData.name}
+    //         onChange={handleInputChange}
+    //         className="w-full p-2 border rounded"
+    //       />
+    //     </div>
+
+    //     <div>
+    //       <label htmlFor="email" className="block mb-2">Your Email</label>
+    //       <input
+    //         type="email"
+    //         id="email"
+    //         name="email"
+    //         value={formData.email}
+    //         onChange={handleInputChange}
+    //         className="w-full p-2 border rounded"
+    //       />
+    //     </div>
+
+    //     <div>
+    //       <label htmlFor="phone" className="block mb-2">Phone Number</label>
+    //       <input
+    //         type="text"
+    //         id="phone"
+    //         name="phone"
+    //         value={formData.phone}
+    //         onChange={handleInputChange}
+    //         className="w-full p-2 border rounded"
+    //         required
+    //       />
+    //     </div>
+              
+    //     <div>
+    //       <label htmlFor="position" className="block mb-2">Applied for position:</label>
+    //       <input
+    //         type="text"
+    //         id="position"
+    //         name="position"
+    //         value={formData.position}
+    //         onChange={handleInputChange}
+    //         className="w-full p-2 border rounded"
+    //         required
+    //       />
+    //     </div>
+
+    //     <div>
+    //       <label htmlFor="figmaLink" className="block mb-2">Figma Link</label>
+    //       <input
+    //         type="url"
+    //         id="figmaLink"
+    //         name="figmaLink"
+    //         value={formData.figmaLink}
+    //         onChange={handleInputChange}
+    //         className="w-full p-2 border rounded"
+    //       />
+    //     </div>
+
+    //     <div>
+    //       <label htmlFor="googleDriveLink" className="block mb-2">Google Drive Link</label>
+    //       <input
+    //         type="url"
+    //         id="googleDriveLink"
+    //         name="googleDriveLink"
+    //         value={formData.googleDriveLink}
+    //         onChange={handleInputChange}
+    //         className="w-full p-2 border rounded"
+    //       />
+    //     </div>
+
+    //     <button
+    //       type="submit"
+    //       className={`px-4 py-2 rounded ${
+    //         submissionExists 
+    //           ? "bg-gray-400 cursor-not-allowed" 
+    //           : "bg-blue-500 hover:bg-blue-600"
+    //       } text-white`}
+    //       title={submissionExists ? "You have already submitted an assignment. Only one submission is allowed." : ""}
+    //       disabled={submissionExists}
+    //     >
+    //       {submissionExists ? "Already Submitted" : "Submit"}
+    //     </button>
+    //   </form>
+    // </div>
   );
 };
 
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import Image from 'next/image';
 import { auth } from "../firebase";
 import LogoutButton from "./LogoutButton";
 
@@ -30,26 +31,45 @@ const NavBar = () => {
     };
   }, []);
 
-  if (loading) {
-    return <nav className="p-4 bg-gray-800 text-white"><p>Loading...</p></nav>;
-  }
-
   if (error) {
-    return <nav className="p-4 bg-gray-800 text-white"><p>Error: {error}</p></nav>;
+    return <nav className="p-4 bg-[#1e1e1e] text-white h-[85px]"><p>Error: {error}</p></nav>;
   }
 
   return (
-    <nav className="p-4 bg-gray-800 text-white flex justify-between items-center">
-      <Link href="/" className="text-xl font-bold">Career Portal</Link>
-      <div>
+    <nav className="p-4 bg-[#1e1e1e] text-white flex justify-between items-center h-[85px]">
+      <Link href="/" className="text-xl font-bold">
+        <img
+          src="https://res.cloudinary.com/dzxx6craw/image/upload/v1730015044/bd7894313133eb84efc2c7974b095307_yvpliu.png"
+          alt="Logo"
+          width={302}
+          height={79}
+        />
+      </Link>
+      <div className="flex items-center justify-center gap-5">
+        <div>
         {user ? (
-          <div className="flex items-center space-x-4">
-            <span>Welcome, {user.displayName || user.email}</span>
+            <div className="flex items-center space-x-4">
+              <div className="w-[60px] h-[60px] rounded overflow-hidden bg-gray-200">
+                <Image
+                  src={user.photoURL || 'https://via.placeholder.com/60x60?text=User'}
+                  alt={user.displayName || "User"}
+                  width={60}
+                  height={60}
+                  className="object-cover"
+                />
+              </div>
+              <div className="flex flex-col">
+                <span className="text-[#9c9ca3]">Welcome,</span>
+                <span className="">{user.displayName || user.email}</span>
+              </div>
             <LogoutButton />
           </div>
         ) : (
-          <Link href="/login" className="text-blue-400 hover:text-blue-300 transition-colors">Login</Link>
+          <Link href="/login" className="bg-white text-[#1e1e1e] rounded-md px-4 py-2 font-bold">
+            Log In
+          </Link>
         )}
+        </div>
       </div>
     </nav>
   );
