1c0b61ae728914601189b0b1aeb0b4c12fc6ec9d
sourav0299 <105085605+sourav0299@users.noreply.github.com>
2024-11-29 16:25:58 +0530

    Refractor: Loader updated

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -5,35 +5,76 @@ const Loader = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <StyledWrapper>
-      <div className="custom-loader" />
-    </StyledWrapper>
+        <div className="loader" />
+      </StyledWrapper>
     </div>
   );
 }
 
 const StyledWrapper = styled.div`
-  .custom-loader {
-    width: 120px;
-    height: 22px;
-    border-radius: 20px;
-    color: #25C1EC;
-    border: 2px solid;
+  .loader {
+    width: 48px;
+    height: 48px;
+    margin: auto;
     position: relative;
   }
 
-  .custom-loader::before {
-    content: "";
+  .loader:before {
+    content: '';
+    width: 48px;
+    height: 5px;
+    background: #25c1ec;
     position: absolute;
-    margin: 2px;
-    inset: 0 100% 0 0;
-    border-radius: inherit;
-    background: #25C1EC;
-    animation: p6 2s infinite;
+    top: 60px;
+    left: 0;
+    border-radius: 50%;
+    animation: shadow324 0.5s linear infinite;
   }
 
-  @keyframes p6 {
+  .loader:after {
+    content: '';
+    width: 100%;
+    height: 100%;
+    background: #25c1ec;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-radius: 4px;
+    animation: jump7456 0.5s linear infinite;
+  }
+
+  @keyframes jump7456 {
+    15% {
+      border-bottom-right-radius: 3px;
+    }
+
+    25% {
+      transform: translateY(9px) rotate(22.5deg);
+    }
+
+    50% {
+      transform: translateY(18px) scale(1, .9) rotate(45deg);
+      border-bottom-right-radius: 40px;
+    }
+
+    75% {
+      transform: translateY(9px) rotate(67.5deg);
+    }
+
     100% {
-      inset: 0
+      transform: translateY(0) rotate(90deg);
+    }
+  }
+
+  @keyframes shadow324 {
+
+    0%,
+      100% {
+      transform: scale(1, 1);
+    }
+
+    50% {
+      transform: scale(1.2, 1);
     }
   }`;
 
