924af3ebaab721a69eb8e36f462e082d3b68bbb8
sourav0299 <105085605+sourav0299@users.noreply.github.com>
2024-12-19 23:51:31 +0530

    Refactor: login page redesign UI

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -40,13 +40,28 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-between gap-5 px-5 pl-40">
+    <div className="relative min-h-screen w-full">
       <Toaster position="top-center" reverseOrder={false} />
-      <div className="flex flex-col items-center justify-center gap-2">
+      <div className="absolute top-5 left-5">
+        <img
+          src="https://res.cloudinary.com/dzxx6craw/image/upload/v1734631568/4b9f9453991fbc560058da9bcc0f169e_uwfeie.png"
+          alt=""
+          className="h-[52px] w-[52px]"
+        />
+      </div>
+
+        <div className="flex flex-col items-center justify-center gap-2">
         <div className="flex flex-col items-center justify-center text-center gap-5">
-          <p className="text-3xl text-[#2f455c] font-bold">Login to your account</p>
-          <p className="text-[15px] text-[#727272] font-bold">Begin Your Journey to a Better Experience</p>
-          <button className="flex border-2 px-20 py-2 bg-[#2f455c] text-white rounded-full" onClick={handleGoogleLogin}>
+          <p className="text-3xl text-[#1e1e1e] font-bold">
+            Login to your account
+          </p>
+          <p className="text-[15px] text-[#727272] font-bold">
+            Begin Your Journey to a Better Experience
+          </p>
+          <button
+            className="flex border-[3px] px-20 py-2 text-[#2F455C] border-[#2F455C] rounded-full"
+            onClick={handleGoogleLogin}
+          >
             <div className="flex items-center justify-center gap-2">
               <FaGoogle className="w-[19px] h-[19px]" />
               <p className="font-semibold">Sign up with google</p>
@@ -54,8 +69,13 @@ const Login = () => {
           </button>
         </div>
       </div>
+
       <div className="flex pt-5">
-        <img src="https://res.cloudinary.com/dzxx6craw/image/upload/v1729856739/d033c17646bbf0a2701cafd12cceb123_eiiq8y.jpg" className="rounded-lg w-[780px] h-[650px]" alt="login-img-banner" />
+        <img
+          src="https://res.cloudinary.com/dzxx6craw/image/upload/v1734631119/0364f2cf65cca8d0737dee39900d431d_a6jcpz.jpg"
+          className="rounded-[20px] w-[840px] h-[700px]"
+          alt="login-img-banner"
+        />
       </div>
     </div>
   );
